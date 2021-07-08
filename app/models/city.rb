860fb0d0fc17e3808cb7cd13cabadca1474c5a63
Nguyen Hoang Mai Phuong <phuongnhm@zigexn.vn>
2021-07-08 11:23:02 +0700

    fix migration

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,4 +1,5 @@
 class City < ApplicationRecord
   has_and_belongs_to_many :jobs
+  has_and_belongs_to_many :companies
   has_many :regions
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,8 @@
 class Job < ApplicationRecord
   has_and_belongs_to_many :users
+  has_and_belongs_to_many :industries
+  has_and_belongs_to_many :cities
   has_many :applies_jobs
   has_many :users, through: :applies_jobs
+  belongs_to :company
 end
