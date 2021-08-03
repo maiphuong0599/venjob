1d61991238493d41b9860f8af64c214eb5826368
Nguyen Hoang Mai Phuong <phuongnhm@zigexn.vn>
2021-08-03 14:17:59 +0700

    fix ID4

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,10 +2,14 @@ class JobsController < ApplicationController
   def show
     if params[:city_slug].present?
       @city = City.find_by(slug: params[:city_slug])
-      @jobs = @city.jobs.latest_jobs.page(params[:page])
+      @jobs = @city.jobs
+      @result = @city.name
     else
       @industry = Industry.find_by(slug: params[:industry_slug])
-      @jobs = @industry.jobs.latest_jobs.page(params[:page])
+      @jobs = @industry.jobs
+      @result = @industry.name
     end
+    @total = @jobs.count
+    @jobs = @jobs.latest_jobs.page(params[:page])
   end
 end
