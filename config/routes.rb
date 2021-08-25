4a80d625a14338fa931ec1d9218fca936fba16e7
Nguyen Hoang Mai Phuong <phuongnhm@zigexn.vn>
2021-08-25 16:34:37 +0700

    fix name routes to redirect right requirement

--- config/routes.rb
+++ config/routes.rb
@@ -9,15 +9,21 @@ Rails.application.routes.draw do
   post 'confirm', to: 'apply_jobs#confirm'
   post 'done', to: 'apply_jobs#done'
   devise_for :users, controllers: { registrations: 'users/registrations', confirmations: 'users/confirmations',
-                                    passwords: 'users/passwords', sessions: 'users/sessions' }
+                                    passwords: 'users/passwords', sessions: 'users/sessions' }, skip: [:sessions, :registrations, :passwords]
   resources :user
   get '/my', to: 'users#show'
   devise_scope :user do
-    get 'register/1', to: 'users/registrations#new'
+    get 'register/1', to: 'users/registrations#new', as: :new_user_registration
     get 'register/2', to: 'users/registrations#show'
-    get 'my/info', to: 'users/registrations#edit'
-    get 'forgot_password', to: 'users/passwords#new'
-    get 'reset_password', to: 'users/passwords#edit'
-    get 'login', to: 'users/sessions#new'
+    get 'my/info', to: 'users/registrations#edit', as: :edit_user_registration
+    match '/my', to: 'devise/registrations#create', as: :user_registration, via: [:post]
+    match '/my', to: 'devise/registrations#update', via: [:put, :patch]
+    get 'forgot_password', to: 'users/passwords#new', as: :new_user_password
+    get 'reset_password', to: 'users/passwords#edit', as: :edit_user_password
+    match 'reset_password', to: 'users/passwords#update', via: [:put, :patch]
+    match 'reset_password', to: 'users/passwords#create', as: :user_password, via: [:post]
+    get 'login', to: 'users/sessions#new', as: :new_user_session
+    post 'login', to: 'users/sessions#create', as: :user_session
+    delete 'logout', to: 'users/sessions#destroy', as: :destroy_user_session, via: Devise.mappings[:user].sign_out_via
   end
 end
