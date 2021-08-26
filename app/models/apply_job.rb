d3fde0de01917d51cc5edf4dbb4ab24a53ac1dc9
Nguyen Hoang Mai Phuong <phuongnhm@zigexn.vn>
2021-08-26 11:06:47 +0700

    fix validate upload file

--- app/models/apply_job.rb
+++ app/models/apply_job.rb
@@ -3,9 +3,8 @@ class ApplyJob < ApplicationRecord
   belongs_to :job
   has_one_attached :cv
   validates :name, presence: true, length: { maximum: 200 }
-  VALID_FILE_UPLOAD = %w[application/msword application/pdf application/xls application/xlsx application/zip].freeze
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
-  validates :cv, presence: true, content_type: { in: VALID_FILE_UPLOAD, message: 'must be a valid pdf format' },
+  validates :cv, presence: true, content_type: { in: 'application/pdf', message: 'must be a valid pdf format' },
                  size: { less_than: 5.megabytes, message: 'should be less than 5MB' }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,11 @@ class User < ApplicationRecord
   has_many :apply_jobs, dependent: :destroy
   has_many :favorite_jobs, dependent: :destroy
   has_many :history_jobs, dependent: :destroy
-  VALID_FILE_UPLOAD = %w[application/msword application/pdf application/xls application/xlsx application/zip].freeze
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable
   validates :name, length: { maximum: 200 }
   validates :email, length: { maximum: 200 }, uniqueness: true
-  validates :cv, content_type: { in: VALID_FILE_UPLOAD, message: 'must be msword, pdf, xls, xlsx, zip format' },
+  validates :cv, content_type: { in: 'application/pdf', message: 'must be a valid pdf format' },
                  size: { less_than: 5.megabytes, message: 'should be less than 5MB' }
 end
