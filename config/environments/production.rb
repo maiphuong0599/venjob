8f04c77c0e055bbc456ae3c5c54ad56660f66d6f
Nguyen Hoang Mai Phuong <phuongnhm@zigexn.vn>
2022-01-25 11:35:43 +0700

    send email with smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "us-cdbr-east-05.cleardb.net" , protocol: "https"} 
+  config.action_mailer.default_url_options = { host: "venjob-recruitment.herokuapp.com" , protocol: "https"} 
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
